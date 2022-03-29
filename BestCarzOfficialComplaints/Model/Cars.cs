6596d661e49236e250dd18184e5c0e6d71d7c239
ogbochinedu <ogbochinedu95@gmail.com>
2022-03-29 21:22:37 +0100

    fix for react

--- BestCarzOfficialComplaints/Model/Cars.cs
+++ BestCarzOfficialComplaints/Model/Cars.cs
@@ -12,6 +12,7 @@ namespace BestCarzOfficialComplaints.Model
         public string Userid { get; set; }
         public string Username { get; set; }
         public string Model { get; set; }
+        public string Brand { get; set; }
         public string Year { get; set; }
         public RepairLog[] RepairLog { get; set; }
       
--- BestCarzOfficialComplaints/Model/Dto/SuccessDto.cs
+++ BestCarzOfficialComplaints/Model/Dto/SuccessDto.cs
@@ -56,11 +56,13 @@ namespace BestCarzOfficialComplaints.Model.Dto
         public string Username { get; set; }
         public string Model { get; set; }
         public string Year { get; set; }
+        public string Brand { get; set; }
     }
     public class CarsDto
     {
         
         public string? _id { get; set; }
+        public string Brand { get; set; }
         public string Userid { get; set; }
         public string Username { get; set; }
         public string Model { get; set; }
